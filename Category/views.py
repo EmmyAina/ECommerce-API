090042f9d0e18fd91e6a2a24bce8d96eacb362dd
Emmanuel Aina <ainae06@gmail.com>
2021-09-07 21:56:21 +0100

    category view

--- Category/views.py
+++ Category/views.py
@@ -1,37 +1,36 @@
-# from Product.models import Product
-# from Product.serializers import Producterializer
+from Product.models import Product
+from Product.serializers import ProductSerializer
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import Category
 from .serializers import CategorySerializer
-
+from core.custompermissions import IsAdminOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
 class CategoryViewSet(ModelViewSet):
 	my_tags = ['Category']
-	# permission_classes = (IsAdminUser,)
+	permission_classes = (IsAdminOrReadOnly,)
 	queryset = Category.objects.all()
 	search_fields = ['category_name', ]
 	filter_backends = [SearchFilter, OrderingFilter]
 	serializer_class = CategorySerializer
 
 
-# class ProductinCategoryView(APIView):
+class ProductinCategoryView(APIView):
 
-# 	"""
-# 	APIView for retrieving a list of Products that belongs to a Category
-# 	by the category id.
-# 	Operation can be carried out any user.
-# 	"""
-# 	my_tags = ['Category']
-# 	permission_classes = (AllowAny,)
+	"""
+	APIView for retrieving a list of Products that belongs to a Category
+	by the category id.
+	Operation can be carried out any user.
+	"""
+	my_tags = ['Category']
+	permission_classes = (AllowAny,)
 
-# 	def get(self, request, id):
-# 		Product = Product.objects.filter(category=id)
+	def get(self, request, id):
+		product = Product.objects.filter(category=id)
 
-# 		ser = Producterializer(Product, many=True)
+		ser = ProductSerializer(product, many=True)
 
-# 		return Response(ser.data)
+		return Response(ser.data)
