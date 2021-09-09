f756da985df31dc95bfaa05f13c9f65caf909252
Emmanuel Aina <ainae06@gmail.com>
2021-09-09 16:58:11 +0100

    cleaned view and models

--- Cart/models.py
+++ Cart/models.py
@@ -5,12 +5,7 @@ from django.conf import settings
 
 
 class Cart(models.Model):
-	customer = models.OneToOneField(
-		settings.AUTH_USER_MODEL,
-		related_name='cart',
-		on_delete=models.CASCADE,
-	)
-
+	customer = models.OneToOneField(settings.AUTH_USER_MODEL,related_name='cart',on_delete=models.CASCADE,)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	def __str__(self):
@@ -19,22 +14,9 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
 
-	product = models.ForeignKey(
-		Product,
-		related_name='cartItem',
-		on_delete=models.CASCADE,
-	)
-
-	cart = models.ForeignKey(
-		Cart,
-		on_delete=models.CASCADE,
-		related_name='cartItem',
-		null=True,
-		blank=True,
-	)
-
+	product = models.ForeignKey(Product,related_name='cartItem',on_delete=models.CASCADE)
+	cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='cartItem',null=True,blank=True,)
 	quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
-
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
@@ -44,29 +26,3 @@ class CartItem(models.Model):
 	def product_total(self):
 		product_total = self.product.product_price_ngn * self.quantity
 		return product_total
-
-# # Create your models here.
-# class Cart(models.Model):
-# 	quantity = models.IntegerField(blank=False)
-# 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-# 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-# 	time_updated = models.DateTimeField(auto_now_add=True)
-
-# 	def __str__(self):
-# 		return f'{self.owner.username} {self.product.product_name}'
-
-# 	def product_total(self):
-# 		product_total = self.product.product_price_ngn * self.quantity
-# 		return product_total
-
-# 	def sub_total(self):
-# 		total_products = len(self)
-# 		items = 0
-# 		sub_total = []
-# 		while items != total_products:
-# 			sub_total.append(self.product_total)
-# 			items+=1
-		
-# 		return sub_total
-		
-
--- Cart/views.py
+++ Cart/views.py
@@ -1,13 +1,7 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from .models import CartItem,Cart
 from .serializers import CartItemSerializer
-from Product.models import Product
-from rest_framework.response import Response
-from rest_framework.exceptions import AuthenticationFailed
-from Utils.utils import get_actual_total
-
 class CartViewSet(ModelViewSet):
 	my_tags = ['Cart']
 	permission_classes = (IsAuthenticated,)
@@ -15,11 +9,4 @@ class CartViewSet(ModelViewSet):
 
 	def get_queryset(self):
 		cart_id = Cart.objects.get(customer_id=self.request.user.id)
-		user_cart = CartItem.objects.filter(cart_id=cart_id)
-		sub_total = get_actual_total(user_cart)
-		print(sub_total)
-
-		cart_id.total = sub_total
-		cart_id.save()
-
 		return CartItem.objects.filter(cart_id=cart_id)
