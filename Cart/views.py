e85882a49f871a4271c72c32fb5d58311dfda017
Emmanuel Aina <ainae06@gmail.com>
2021-09-09 10:09:54 +0100

    cart viewset

--- Cart/views.py
+++ Cart/views.py
@@ -6,36 +6,20 @@ from .serializers import CartItemSerializer
 from Product.models import Product
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from Utils.utils import get_actual_total
 
-
-class AddItemToCart(ModelViewSet):
+class CartViewSet(ModelViewSet):
 	my_tags = ['Cart']
 	permission_classes = (IsAuthenticated,)
 	serializer_class = CartItemSerializer
 
 	def get_queryset(self):
 		cart_id = Cart.objects.get(customer_id=self.request.user.id)
-		print(cart_id)
+		user_cart = CartItem.objects.filter(cart_id=cart_id)
+		sub_total = get_actual_total(user_cart)
+		print(sub_total)
+
+		cart_id.total = sub_total
+		cart_id.save()
+
 		return CartItem.objects.filter(cart_id=cart_id)
-	"""
-	APIView that takes a product id and adds it to the current user's cart,
-	it collects the quantity of products that needs to be added to the cart
-	"""
-
-	def post(self, request):
-
-		current_cart = Cart.objects.get(customer=request.user.id)
-		print(request.user.id)
-		try:
-			product_to_add = Product.objects.get(id=request.data['product_id'])
-		except Product.DoesNotExist:
-			raise AuthenticationFailed("Product does not exist")
-		add_item = CartItem(cart=current_cart,
-                      product_id=request.data['product_id'], quantity=request.data['quantity'])
-		serialized_item = CartItemSerializer(data=request.data)
-		if serialized_item.is_valid():
-			add_item.save()
-
-		return Response(serialized_item.data)
-
-
