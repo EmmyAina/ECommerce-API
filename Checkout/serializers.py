38c0b795d1b4d1806a89f12eb943a9ade7a35e8d
Emmanuel Aina <ainae06@gmail.com>
2021-09-09 17:04:21 +0100

    added serializer for collecting payment details

--- Checkout/serializers.py
+++ Checkout/serializers.py
@@ -1,11 +1,13 @@
+from Checkout.models import Coupon
 from rest_framework import serializers
-from Cart.models import Cart
+from Cart.models import CartItem
 
-class OrderItemSerializer(serializers.ModelSerializer):
+
+class ApplyCouponSerializer(serializers.ModelSerializer):
     coupon = serializers.CharField(max_length=7)
 
     class Meta:
-        model = Cart
+        model = CartItem
         fields = ('coupon',)
 
     def validate(self, data):
@@ -17,3 +19,18 @@ class OrderItemSerializer(serializers.ModelSerializer):
         data['coupon_value'] = coupoun_value
 
         return data
+
+class CouponSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Coupon
+		fields = '__all__'
+
+class PaymentDetailSerializer(serializers.Serializer):
+	cardnumber = serializers.CharField(max_length=16)
+	email = serializers.CharField(max_length=255)
+	cvv = serializers.CharField(max_length=3)
+	exp_month = serializers.CharField(max_length=2)
+	exp_year = serializers.CharField(max_length=4)
+	address_line1 = serializers.CharField(max_length=255)
+	address_state = serializers.CharField(max_length=255)
+	address_country = serializers.CharField(max_length=255)
