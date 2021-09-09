54fa80c674b32ab2554b46d1c5b5487e897bbcca
Emmanuel Aina <ainae06@gmail.com>
2021-09-09 16:59:22 +0100

    added new models to dadmin

--- Checkout/admin.py
+++ Checkout/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon,TrackCoupon,CheckedOutOrder
 # Register your models here.
-admin.site.register(Coupon)
+admin.site.register((Coupon, TrackCoupon, CheckedOutOrder))
