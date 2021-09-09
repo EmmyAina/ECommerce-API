ae991f072bfe4b86a41dd00f7ae0a9a27a5ab49f
Emmanuel Aina <ainae06@gmail.com>
2021-09-09 17:03:43 +0100

    models for checking out,tracking coupon usage...

--- Checkout/models.py
+++ Checkout/models.py
@@ -1,4 +1,9 @@
+from django.db.models.fields.related import ForeignKey
+from Cart.serializers import CartProductSerializer
+from Cart.models import Cart
 from django.db import models
+import uuid
+from Account.models import User
 
 class Coupon(models.Model):
 	code = models.CharField(max_length=7, unique=True)
@@ -7,6 +12,9 @@ class Coupon(models.Model):
 	active = models.BooleanField(default=True)
 	num_available = models.IntegerField(default=1)
 	num_used = models.IntegerField(default=0)
+	one_time_use = models.BooleanField(default=True)
+	date_added = models.DateTimeField(auto_now=True)
+	expiry_date = models.DateTimeField()
 
 	def __str__(self):
 		return self.code
@@ -23,7 +31,35 @@ class Coupon(models.Model):
 
 	def use_coupon(self):
 		self.num_used += 1
-		if self.num_used == self.num_available:
+		if self.num_used >= self.num_available:
 			self.active = False
 		
 		self.save()
+
+	def has_been_redeemed(self,user):
+		been_redeemed = False
+		tracked_coupons = TrackCoupon.objects.filter(customer=user)
+		used_codes = []
+		for tracled_coupon in tracked_coupons:
+			used_codes.append(tracled_coupon.coupon.code)
+		if self.code in used_codes:
+			been_redeemed = True
+		return been_redeemed
+class CheckedOutOrder(models.Model):
+	purchase_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+	coupon_applied = models.ForeignKey(Coupon, on_delete=models.CASCADE, default=None, blank=True, null=True)
+	sub_total = models.DecimalField(max_digits=10, decimal_places=2)
+	discounted_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+	success = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f'{self.cart.customer.email} OrderPlaced: {self.success}'
+
+class TrackCoupon(models.Model):
+	customer = models.ForeignKey(User, on_delete=models.CASCADE)
+	coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f'{self.customer.email}: {self.coupon.code}'
