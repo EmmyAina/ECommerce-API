fd883ab58bc00a78cc04333b956f5bbf6dab5458
Emmanuel Aina <ainae06@gmail.com>
2021-09-10 09:13:50 +0100

    modified queryset

--- Cart/views.py
+++ Cart/views.py
@@ -6,7 +6,8 @@ class CartViewSet(ModelViewSet):
 	my_tags = ['Cart']
 	permission_classes = (IsAuthenticated,)
 	serializer_class = CartItemSerializer
+	queryset = CartItem.objects.all()
 
 	def get_queryset(self):
-		cart_id = Cart.objects.get(customer_id=self.request.user.id)
-		return CartItem.objects.filter(cart_id=cart_id)
+		cart= Cart.objects.get(customer_id=self.request.user.id)
+		return self.queryset.filter(cart_id=cart)
