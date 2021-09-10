10856b7d70905abf4e6d0bd5c5664c1c7c37ac4f
Emmanuel Aina <ainae06@gmail.com>
2021-09-10 16:54:41 +0100

    get products in category

--- Category/urls.py
+++ Category/urls.py
@@ -8,6 +8,6 @@ router.register('category', views.CategoryViewSet, basename='category')
 
 urlpatterns = [
 	path('api/v1/', include(router.urls), name='category'),
-	# path('api/v1/post-in-category/<int:id>/',
-	#      views.PostsinCategoryView.as_view(), name='post-in-category')
+	path('api/v1/category/<int:id>/products/',
+	     views.ProductsinCategoryView.as_view(), name='post-in-category')
 ]
