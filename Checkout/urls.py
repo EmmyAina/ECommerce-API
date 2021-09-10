c1a5279abf13cbd58da64591935eb87d5b92d907
Emmanuel Aina <ainae06@gmail.com>
2021-09-10 17:12:03 +0100

    default routers for new views

--- Checkout/urls.py
+++ Checkout/urls.py
@@ -3,18 +3,22 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-# router.register('checkout', views.CheckoutCart, 'cart')
+router.register('coupons', views.CouponViewSet, 'coupons')
+router.register('checkout/items',views.CheckedOutItemsViewset, 'checked-out-items')
+# router.register('cart/checkout/', views.PerformCheckout, 'checkout-cart')
+
 
 # urlpatterns = [
-# 	path('api/v1/', include(router.urls))
+#     path('api/v1/cart/checkout/list',
+#          views.CheckoutCartItems.as_view(), name='order-from-cart'),
+#     path('api/v1/cart/checkout',
+#          views.Checkout.as_view(), name='order-from-cart'),
+
 # ]
 urlpatterns = [
-    path('api/v1/cart/checkout/list',
-         views.CheckoutCartItems.as_view(), name='order-from-cart'),
-    path('api/v1/cart/checkout',
-         views.Checkout.as_view(), name='order-from-cart'),
-    path('api/v1/cart/coupons',
-         views.CouponViewsSet.as_view(), name='order-from-cart'),
+    path('api/v1/', include(router.urls)),
     path('api/v1/cart/order',
          views.MakePaymentView.as_view(), name='order-from-cart'),
+    path('api/v1/cart/checkout',
+        views.Checkout.as_view(), name='order-from-cart'),
 ]
