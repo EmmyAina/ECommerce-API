8e3d9d37053cd3c4ff14bdad8207ea0769768a03
Emmanuel Aina <ainae06@gmail.com>
2021-09-11 13:14:44 +0100

    verify email after registration

--- Account/views.py
+++ Account/views.py
@@ -5,12 +5,15 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import FormParser, MultiPartParser
 from Account.models import User, UserBio
-from Account.serializers import (UserBioSerializers,
-                                 UserSerializers)
-from Cart.models import Cart
-
+from Account.serializers import (UserBioSerializers,UserSerializers)
+import jwt
+from rest_framework_simplejwt.tokens import RefreshToken
+from .utils import Utils
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from decouple import config
 class RegisterView(GenericAPIView):
-	my_tags = ['Auth']
+	my_tags = ['User']
 	permission_classes = (AllowAny,)
 	serializer_class = UserSerializers
 
@@ -19,15 +22,57 @@ class RegisterView(GenericAPIView):
 		serialized_data = UserSerializers(data=request.data)
 		serialized_data.is_valid(raise_exception=True)
 		serialized_data.save()
+		
+		# Verify Email
+		user_data = serialized_data.data
+		user = User.objects.get(email=user_data['email'])
+		token = RefreshToken.for_user(user).access_token
+
+		current_site = get_current_site(request).domain
+		relative_link = reverse('email-verify')
+		abs_url = f"http://{current_site}{relative_link}?token={str(token)}"
+		email_body = f"Hi {user.username}, Use the link below to verify your email \n {abs_url}"
+		data = {"body": email_body, "subject": 'Verify your email',
+                    "recepient": [user.email], "sender": config("ECOMM_MAILGUNEMAIL")}
+		Utils.send_verification_mail(data)
+
 
 		response.data = {
 			'success': True,
 			'id': serialized_data.data['id'],
-			'message': "Account created successfully",
+			'message': "Account created successfully. Check your mail to verify your account",
 		}
 		response.status = status.HTTP_201_CREATED
 		
 		return response
+
+class VerifyEmail(GenericAPIView):
+	my_tags = ['User']
+
+	def get(self, request):
+		token = request.GET.get("token")
+		response = Response()
+		try:
+			payload = jwt.decode(token, config("ECOMM_SECRETKEY"), algorithms=["HS256"])
+			user = User.objects.get(id=payload['user_id'])
+			if not user.verified:
+				user.verified = True
+				user.save()
+				response.data = {
+					"detail": "Account verified successfully. Please proceed to login"}
+				response.status = status.HTTP_200_OK
+				return response
+
+		except jwt.ExpiredSignatureError as identifier:
+			response.data = {"error": "Verification Token Expired. Please try again"}
+			response.status = status.HTTP_400_BAD_REQUEST
+			return response
+
+		except jwt.DecodeError as identifier:
+			response.data = {"error": "Invalid Verification Token. Please try again"}
+			response.status = status.HTTP_400_BAD_REQUEST
+
+			return response
 
 class UpdateBioViewSet(ModelViewSet):
 	my_tags = ['User-Profile']
