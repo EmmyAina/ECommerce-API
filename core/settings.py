de869218dc01ccbda2fd345f8e4c1fb53721eba0
Aina Emmanuel <ainae06@gmail.com>
2021-09-12 21:08:06 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -94,20 +94,31 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-     			'NAME': config("ECOMMNAME"),
-     			'USER': config("ECOMMUSER"),
-     			'PASSWORD': config("ECOMMPASSWORD"),
-     			'HOST': "localhost",
-     			'PORT': '5432',
-     			'TEST': {
-                            'NAME': 'first_test_db',
-                        },
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions_db',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
     }
-}
+else:
+	DATABASES = {
+	    'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+				'NAME': config("ECOMMNAME"),
+				'USER': config("ECOMMUSER"),
+				'PASSWORD': config("ECOMMPASSWORD"),
+				'HOST': "localhost",
+				'PORT': '5432',
+				'TEST': {
+				    'NAME': 'first_test_db',
+				},
+	    }
+	}
 
 AUTH_USER_MODEL = 'Account.user'
 
