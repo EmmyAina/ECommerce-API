208777a1e22d0ee383da6cec67a6810a61f8e7e4
Emmanuel Aina <ainae06@gmail.com>
2021-09-25 18:08:15 +0100

    added RN host to allowed cors

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-  	'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -193,6 +194,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', "http://localhost:19006"]
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Mailing Informaitions
 EMAIL_HOST = 'smtp.mailgun.org'
